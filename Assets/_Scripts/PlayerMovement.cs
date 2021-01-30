7860ee300e0298f44576e51262cd528eee92d6dc
DanielGCh1 <58349941+DanielGCh1@users.noreply.github.com>
2021-01-30 05:00:28 -0600

    Jugabilidad

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    private bool tieneBandera = false;
+    public float vida = 100f;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // INPUT
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // MOVEMENT
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (tieneBandera) {
+            GameObject.Find("Bandera").GetComponent<Rigidbody2D>().MovePosition(new Vector2(rb.position.x + 2f, rb.position.y + 2f) + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+    public void setTieneBandera(bool tienBand)
+    {
+        tieneBandera = tienBand;
+    }
+    public bool getTieneBandera() {
+        return tieneBandera;
+    }
+}
