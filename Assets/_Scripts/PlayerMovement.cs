7dd6ea3ee522ee291823127c6ba2fe925234bab3
DanielGCh1 <58349941+DanielGCh1@users.noreply.github.com>
2021-01-30 11:37:03 -0600

    IntegracionDeScrips

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -6,10 +6,24 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    private bool tieneBandera = false;
+    public float vida = 100f;
+
+    public Animator animator;
+    public List<string> animations = new List<string>();
+    static AudioSource sound;
+    public List<AudioClip> audios = new List<AudioClip>();
+
 
     Vector2 movement;
 
     // Update is called once per frame
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sound = GetComponent<AudioSource>();
+    }
     void Update()
     {
         // INPUT
@@ -21,6 +35,17 @@ public class PlayerMovement : MonoBehaviour
     {
         // MOVEMENT
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
+        if (tieneBandera)
+        {
+            GameObject.Find("Bandera").GetComponent<Rigidbody2D>().MovePosition(new Vector2(rb.position.x + 2f, rb.position.y + 2f) + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+    public void setTieneBandera(bool tienBand)
+    {
+        tieneBandera = tienBand;
+    }
+    public bool getTieneBandera()
+    {
+        return tieneBandera;
     }
 }
