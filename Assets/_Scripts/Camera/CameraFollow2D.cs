78ac424f2025efbd21b49ced5455cdf85d2adfd9
= <juanlgu27@gmail.com>
2021-01-30 05:25:14 -0600

    integrando photon

---
+++ Assets/_Scripts/Camera/CameraFollow2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    public Transform Player;
+    public float CameraDistance = 5.0f;
+
+    private void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / CameraDistance);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector2(Player.position.x, Player.position.y);
+    }
+}
